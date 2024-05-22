486c182d9997c848a086af2789d0abb098e4f79c
Filipe Correia <frtcorreia@gmail.com>
2024-05-22 08:35:46 +0100

    added cors and default route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import express from "express";
-import { json } from "body-parser";
+import express, { Router, Request, Response } from "express";
+import cors from "cors";
 import controllersController from "./controllers/capture.controller";
 import { authMiddleware } from "./middlewares/auth.middleware";
 import authRoutes from "./routes/auth.routes";
 
 const app = express();
+const route = Router();
 
-app.use(json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/auth", authRoutes);
 app.use("/capture", authMiddleware, controllersController);
+
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to FishLog API" });
+});
+
+app.use(route);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
